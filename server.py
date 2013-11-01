3087ddfa9a6ee06d133ef2a01ed94afcc630213d
tungul <lugnut1206@gmail.com>
2013-11-01 10:48:48 -0700

    began work. i have no idea how python handles webservers...

---
+++ server.py
@@ -0,0 +1,40 @@
+import threading, sockets
+
+shutdown = False
+
+# create an array or something of existing files in the screenshots directory, for comparison later.
+
+class Webserver(threading.Thread): # figure out how the hell webservers work in python
+	def __init__(self):
+		threading.Thread.__init__(self)
+
+	def run(self):
+		while True: # code here
+			if shutdown == True:
+				break
+
+class ScreenshotMonitor(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+
+	def run(self):
+		while True:
+			try:
+				if shutdown == True:
+					break
+				# check if a new file exists, then move it out
+			except: # some error
+				continue
+
+webServer = Webserver()
+screenShotMonitor = ScreenshotMonitor()
+webServer.start()
+screenShotMonitor.start()
+
+while True:
+	try:
+		pass
+	except KeyboardInterrupt:
+		shutdown = True
+
+quit()
