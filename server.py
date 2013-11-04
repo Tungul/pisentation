df6462efea926d6363f1cb94a6b23ddaf79471fd
tungul <lugnut1206@gmail.com>
2013-11-03 21:15:29 -0800

    added notes for documentation lookup

--- server.py
+++ server.py
@@ -2,14 +2,14 @@ import threading, sockets
 
 shutdown = False
 
-# create an array or something of existing files in the screenshots directory, for comparison later.
+# create an array or list or something of existing files in the screenshots directory, for comparison later.
 
 class Webserver(threading.Thread): # figure out how the hell webservers work in python
 	def __init__(self):
 		threading.Thread.__init__(self)
 
 	def run(self):
-		while True: # code here
+		while True: # code here # http://docs.python.org/2/howto/sockets.html
 			if shutdown == True:
 				break
 
@@ -22,7 +22,7 @@ class ScreenshotMonitor(threading.Thread):
 			try:
 				if shutdown == True:
 					break
-				# check if a new file exists, then move it out
+				# check if a new file exists, then move it out http://docs.python.org/2/library/filecmp.html
 			except: # some error
 				continue
 
